c29865e5f9108bda5d98b1b24b23631ce2cb5bee
Samin Shams <saminshams@gmail.com>
2018-08-04 16:03:09 -0300

    new task working

--- app/screens/NewTask.js
+++ app/screens/NewTask.js
@@ -2,19 +2,69 @@ import React from 'react';
 import {
   View,
   TouchableOpacity,
-  Text
+  Text,
+  TextInput,
+  StyleSheet
 } from 'react-native';
 
+import Rating from '../components/Rating';
+
+import MyStorage from '../libs/Storage';
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+    padding: 5
+  }
+});
+
 export default class NewTask extends React.Component {
+  state = {
+    title: '',
+    description: '',
+    rating: 1
+  }
+
+  saveTask = async () => {
+    const task = this.state;
+
+    await new MyStorage().add(task);
+
+    this.props.navigation.goBack();
+  }
+
   render () {
     return (
       <View>
-        <Text>New Task</Text>
+        <Text style={{ fontSize: 30 }}>New Task</Text>
         <TouchableOpacity
           onPress={() => this.props.navigation.goBack()}
         >
           <Text>Back</Text>
         </TouchableOpacity>
+        <View style={{ padding: 20 }}>
+          <Text>Title</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Title"
+            onChangeText={(text) => this.setState({ title: text })}
+          />
+          <Text>Description</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Description"
+            onChangeText={(text) => this.setState({ description: text })}
+          />
+          <Rating
+            ratValue={this.state.rating}
+            onPressRating={(index, value) => this.setState({ rating: value })}
+          />
+          <TouchableOpacity
+            onPress={this.saveTask}
+          >
+            <Text>Save</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
